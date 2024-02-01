b5ca55a6abfc26052e7da58f7315592737e7763a
TerreDHermes <vikden.99@mail.ru>
2024-02-01 19:31:15 +0300

    Создана архитектурная база. 3 слоя и подключение к бд.

---
+++ pkg/handler/handler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"Infotecs/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	return router
+}
